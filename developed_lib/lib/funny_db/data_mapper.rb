363c742bba3d32486cc7060b49542feefd8e5b07
VanyaZ158 <VanyaZ158@gmail.com>
2016-06-01 07:46:55 +0300

    Comment class DataMapper and realize full CRUD interface with this class

--- developed_lib/lib/funny_db/data_mapper.rb
+++ developed_lib/lib/funny_db/data_mapper.rb
@@ -1,3 +1,11 @@
+##
+# This class realize CRUD operations on database's items
+# C - #insert_one(data_to_insert) and #insert_list(array_of_data_to_insert),
+# where data_to_insert is a hash with structure you want,
+# and array_of_data_to_insert - is a just array of data_to_insert
+# R - #[index], where index - number from 0 to (infinity and beyond)
+# U - update_at(index), where index - number from 0 to (infinity and beyond)
+# D - delete_at(index), where index - number from 0 to (infinity and beyond)
 module FunnyDb
   class DataMapper
     def initialize(group_name, initializer)
@@ -31,6 +39,20 @@ module FunnyDb
       array_of_data_to_insert.each { |ins_data| insert_one(ins_data) }
 
       last_index
+    end
+
+    def update_at(index)
+      yield @changed_data[index]
+
+      unless @changed_data[index].is_a? Hash
+        raise DataMapperError, 'Changed item must be a hash'
+      end
+
+      Marshal.load(Marshal.dump(@changed_data[index]))
+    end
+
+    def delete_at(index)
+      @changed_data.delete_at index
     end
 
     def drop_all_session_changes
